f6e91eeea81d93f031651fa9ea65fd08553a0cc4
JordanHay <51302288+JordanHay@users.noreply.github.com>
2020-12-07 09:06:27 -0500

    added all
    commit all

---
+++ HotelApp.Desktop - Copy/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IDatabaseData _db;
+        public MainWindow(IDatabaseData db)
+        {
+            InitializeComponent();
+            _db = db;
+        }
+
+        private void searchForGuest_Click(object sender, RoutedEventArgs e)
+        {
+            List<BookingFullModel> bookings =_db.SearchBookings(lastNameText.Text);
+            resultsList.ItemsSource = bookings;
+        }
+        private void CheckInButton_Click(object sender, RoutedEventArgs e)
+        {
+            var checkInForm = App.serviceProvider.GetService<CheckInForm>();
+            var model = (BookingFullModel)((Button)e.Source).DataContext;
+            checkInForm.PopulatedCheckInInfo(model);
+            checkInForm.Show();
+        }
+    }
+}
---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Databases;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider serviceProvider;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<CheckInForm>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddTransient<IDatabaseData, SqlData>();
+            
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+
+            IConfiguration config = builder.Build();
+
+            services.AddSingleton(config);
+
+            serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }
+    }
+}
